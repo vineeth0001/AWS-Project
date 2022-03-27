3874d11510a48e3ebd7f08adb728f5846dbb3a71
vineeth0001 <kvkvr01@gmail.com>
2022-03-27 13:06:19 +0530

    Aws

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "db.cnngdrbdps1w.ap-south-1.rds.amazonaws.com"
+customuser = "Admin"
+custompass = "rainbow123"
+customdb = "DB"
+custombucket = "goemployee"
+customregion = "us-east-2"
